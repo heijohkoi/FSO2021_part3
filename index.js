8e7249b5689072b7acbcbdfd7bf50f657e4420cf
Heikki Koivula <heikki.koivula@gmail.com>
2021-06-13 21:10:52 +0300

    update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
 
+app.use(express.static("build"));
 app.use(express.json());
-
 app.use(cors());
 
 app.use(
