e9bdebd4aa4879228100a8ef19e8a792c595e1b6
Heikki Koivula <heikki.koivula@gmail.com>
2021-06-15 10:25:31 +0300

    exercise 3.14 complete

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const { request } = require("express");
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express');
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { request } = require('express');
+const morgan = require('morgan');
+const Person = require('./models/person');
 
-app.use(express.static("build"));
-app.use(express.json());
+app.use(express.static('build'));
 app.use(cors());
+app.use(express.json());
 
 app.use(
   morgan((tokens, req, res) => {
@@ -14,106 +16,90 @@ app.use(
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
       JSON.stringify(req.body),
-    ].join(" ");
-  })
+    ].join(' ');
+  }),
 );
 
+/*
 let persons = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
+    name: 'Arto Hellas',
+    number: '040-123456',
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
+    name: 'Dan Abramov',
+    number: '12-43-234345',
   },
   {
     id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122",
+    name: 'Mary Poppendick',
+    number: '39-23-6423122',
   },
 ];
+*/
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then((result) => {
+    res.json(result);
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+  });
 });
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
-});
-
-app.get("/info", (req, res) => {
+/*
+app.get('/info', (req, res) => {
   res.send(
     `<p>Phonebook has info for ${
       persons.length
-    } people<p><p>${new Date().toString()}<p>`
+    } people<p><p>${new Date().toString()}<p>`,
   );
 });
+*/
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.get('/api/persons/:id', (req, res) => {
+  Person.findById(req.params.id).then((person) => res.json(person));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
+/*
+app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter((person) => person.id !== id);
 
   res.status(204).end();
 });
+*/
 
-app.post("/api/persons", (req, res) => {
+app.post('/api/persons', (req, res) => {
   const body = req.body;
 
-  if (!body.name) {
-    return res.status(400).json({
-      error: "name missing",
-    });
-  } else if (!body.number) {
-    return res.status(400).json({
-      error: "number missing",
-    });
-  } else if (
-    persons.find(
-      (person) =>
-        person.name.toLocaleLowerCase() === body.name.toLocaleLowerCase()
-    )
-  ) {
-    return res.status(400).json({
-      error: "name must be unique",
-    });
+  if (body.name === undefined) {
+    return res.status(400).json({ error: 'content missing' });
   }
 
-  const person = {
-    id: Math.floor(Math.random() * 10000000),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  res.json(person);
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
